2b7d20947a503b1c14ba980648b03f938ddb6916
BabaYaga <danyl.mota@gmail.com>
2024-02-15 10:26:45 +0100

    #5 list of players on team model

--- flutter_table_football/lib/src/data/models/team.model.dart
+++ flutter_table_football/lib/src/data/models/team.model.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_table_football/src/data/models/player.model.dart';
 
 /// Team Model
 ///
@@ -32,6 +38,7 @@ class Team {
   final int points;
   final int goalsFor;
   final int goalsAgainst;
+  final List<Player> players;
 
   const Team({
     required this.id,
@@ -42,7 +49,20 @@ class Team {
     required this.points,
     required this.goalsFor,
     required this.goalsAgainst,
+    required this.players,
   });
+
+  /// Computed property for the team's games
+  int get match => wins + losses + ties;
+
+  /// Computed property for the team's win percentage.
+  double get ration => match / wins;
+
+  /// Get the number of goals between the Goals for and against the team.
+  int get goalsDiference => (goalsFor - goalsAgainst).abs();
+
+  /// Computed property to be used during the search process
+  String get searchable => name.toLowerCase();
 
   Team copyWith({
     int? id,
@@ -53,6 +73,7 @@ class Team {
     int? points,
     int? goalsFor,
     int? goalsAgainst,
+    List<Player>? players,
   }) {
     return Team(
       id: id ?? this.id,
@@ -63,6 +84,7 @@ class Team {
       points: points ?? this.points,
       goalsFor: goalsFor ?? this.goalsFor,
       goalsAgainst: goalsAgainst ?? this.goalsAgainst,
+      players: players ?? this.players,
     );
   }
 
@@ -76,10 +98,17 @@ class Team {
       'points': points,
       'goalsFor': goalsFor,
       'goalsAgainst': goalsAgainst,
+      'players': players.map((x) => x.toMap()).toList(),
     };
   }
 
   factory Team.fromMap(Map<String, dynamic> map) {
+    // get the players
+    List<Player> players = List.empty(growable: true);
+    for (var player in map['players']) {
+      players.add(Player.fromMap(player));
+    }
+
     return Team(
       id: map['id'] as int,
       name: map['name'] as String,
@@ -89,32 +118,23 @@ class Team {
       points: map['points'] as int,
       goalsFor: map['goalsFor'] as int,
       goalsAgainst: map['goalsAgainst'] as int,
+      players: players,
     );
   }
 
-  /// Computed property for the team's games
-  int get match => wins + losses + ties;
-
-  /// Computed property for the team's win percentage.
-  double get ration => match / wins;
-
-  /// Get the number of goals between the Goals for and against the team.
-  int get goalsDiference => (goalsFor - goalsAgainst).abs();
-
   @override
   String toString() {
-    return 'Team(id: $id, name: $name, wins: $wins, losses: $losses, ties: $ties, points: $points, goalsFor: $goalsFor, goalsAgainst: $goalsAgainst)';
+    return 'Team(id: $id, name: $name, wins: $wins, losses: $losses, ties: $ties, points: $points, goalsFor: $goalsFor, goalsAgainst: $goalsAgainst, players: $players)';
   }
 
   @override
   bool operator ==(covariant Team other) {
     if (identical(this, other)) return true;
-
-    return other.id == id && other.name == name && other.wins == wins && other.losses == losses && other.ties == ties && other.points == points && other.goalsFor == goalsFor && other.goalsAgainst == goalsAgainst;
+    return other.id == id && other.name == name && other.wins == wins && other.losses == losses && other.ties == ties && other.points == points && other.goalsFor == goalsFor && other.goalsAgainst == goalsAgainst && listEquals(other.players, players);
   }
 
   @override
   int get hashCode {
-    return id.hashCode ^ name.hashCode ^ wins.hashCode ^ losses.hashCode ^ ties.hashCode ^ points.hashCode ^ goalsFor.hashCode ^ goalsAgainst.hashCode;
+    return id.hashCode ^ name.hashCode ^ wins.hashCode ^ losses.hashCode ^ ties.hashCode ^ points.hashCode ^ goalsFor.hashCode ^ goalsAgainst.hashCode ^ players.hashCode;
   }
 }
