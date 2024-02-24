9be2dd403c45618988c91b842be10fb34be5a28a
BabaYaga <danyl.mota@gmail.com>
2024-02-24 18:32:14 +0100

    removing unnecessary  log

--- api_table_football/app/Providers/AppServiceProvider.php
+++ api_table_football/app/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Scramble::routes(function (Route $route) {
-            Log::alert($route->uri);
             return Str::startsWith($route->uri, 'api/');
         });
     }
