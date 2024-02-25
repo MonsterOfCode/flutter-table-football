14b33e82ced46319661c4a664b3384d4f52b65a9
BabaYaga <danyl.mota@gmail.com>
2024-02-25 17:53:26 +0100

    #40 handling 404 on phone working but seems not on web

--- releases/flutter/001/flutter_service_worker.js
+++ releases/flutter/001/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "b534e15a4230277bf21465ab3f7a6484",
-"index.html": "cb4728ff7867efcdf2240a0040eeb0d4",
-"/": "cb4728ff7867efcdf2240a0040eeb0d4",
-"main.dart.js": "8da902c62745efe9766ca577ae6a9568",
+"index.html": "c673af03a801212080115ef157f08cf3",
+"/": "c673af03a801212080115ef157f08cf3",
+"main.dart.js": "29b9377b9606726eb79b6adbb7ed2d19",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
